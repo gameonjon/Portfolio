59f05ebbdd75da482d7e82b59d97c522fe5dc790
gameonjon <39804998+gameonjon@users.noreply.github.com>
2021-05-20 03:22:29 -0700

    insert js file

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-var figure = $(".video").hover(hoverVideo, hideVideo);
+$(document).ready(function() {
+    $('a[href*=#').bind('click', function(e) {
+        e.preventDefault(); //prevent hard jump, default behavior
 
-function hoverVideo(e) {
-    $('video', this).get(0).play();
-}
+        var target = $(this).attr("href");  //set target as variable
 
-function hideVideo(e) {
-    $('video', this).get(0).pause();
-}
+        //perform animated scroll by getting top-position of target element and set as scroll target
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600, function() {
+            location.hash = target; //attach hash (#jumpTarget) to the pageURL
+        });
+        return false;
+    });
+});
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+    //assign active class to nav links while scrolling
+    $('.flex-container').each(function(i) {
+        if ($(this).position().top <= scrollDistance){
+            $('.nav a.active').removeClass('active');
+            $('.nav a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
